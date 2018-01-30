8313acff4e8f1df8b1a62241d158616ec8302493
alehuo <aleksi.huotala@helsinki.fi>
2018-01-30 13:57:27 +0200

    Fixed item price, as back-end returns cents, not euros

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -6,22 +6,19 @@ import Margin from "../helpers/Margin";
 
 import "./styles/Header.css";
 
+const item = {
+  price: 180,
+  name: "Coca-Cola Zero 0.5l"
+};
+
 export class Header extends React.Component {
   render() {
     return (
       <header>
         <div className="header-left">
           <Margin margin={5} inlineBlock>
-            <BasicBtn
-              hover
-              onClick={() =>
-                this.props.buy({
-                  price: 2,
-                  name: "Coca-Cola Zero 0.5l"
-                })
-              }
-            >
-              Testiostos (2 &euro;)
+            <BasicBtn hover onClick={() => this.props.buy(item)}>
+              Testiostos ({parseFloat(item.price / 100).toFixed(2)} &euro;)
             </BasicBtn>
           </Margin>
         </div>
@@ -30,7 +27,8 @@ export class Header extends React.Component {
             <BasicBtn>
               <span>
                 <b>{this.props.user.full_name}</b>{" "}
-                {parseFloat(this.props.user.account_balance).toFixed(2)} &euro;
+                {parseFloat(this.props.user.account_balance / 100).toFixed(2)}{" "}
+                &euro;
               </span>
             </BasicBtn>
           </Margin>
