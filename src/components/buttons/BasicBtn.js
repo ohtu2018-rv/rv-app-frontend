a42ff1819ad7c3a7ce9908111ec9208019765144
alehuo <aleksi.huotala@helsinki.fi>
2018-02-04 11:21:31 +0200

    Fixed Basic button hover effect

--- src/components/buttons/BasicBtn.js
+++ src/components/buttons/BasicBtn.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./styles/Button.css";
 import "./styles/BasicBtn.css";
 import Loader from "./../loaders/Loader";
+import PropTypes from "prop-types";
 
+/**
+ * Basic button.
+ *
+ * Suitable for UI actions, that do not change the current page or execute a critical action, such as submitting a form.
+ */
 const BasicBtn = ({ onClick, children, fill, hover, loader, ...props }) => {
   let className = "btn";
   fill ? (className += " basic-fill") : (className += " basic");
@@ -14,6 +20,24 @@ const BasicBtn = ({ onClick, children, fill, hover, loader, ...props }) => {
       {!loader ? <span className="btnContent">{children}</span> : <Loader />}
     </button>
   );
+};
+
+BasicBtn.propTypes = {
+  /** Click handler. */
+  onClick: PropTypes.func,
+  /** Button fill. If set to `true`, the button's colors will "invert". */
+  fill: PropTypes.bool,
+  /** Hover effect. If set to `true`, the button will have a hover effect. */
+  hover: PropTypes.bool,
+  /** Loader effect. If set to `true`, will show a CSS loader instead of button text. */
+  loader: PropTypes.bool
+};
+
+BasicBtn.defaultProps = {
+  onClick: null,
+  fill: false,
+  hover: false,
+  loader: false
 };
 
 export default BasicBtn;
--- src/components/buttons/SuccessBtn.js
+++ src/components/buttons/SuccessBtn.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/Button.css";
 import "./styles/SuccessBtn.css";
 import Loader from "./../loaders/Loader";
+import PropTypes from "prop-types";
 
 const SuccessBtn = ({ onClick, children, fill, hover, loader, ...props }) => {
   let className = "btn";
@@ -16,6 +17,24 @@ const SuccessBtn = ({ onClick, children, fill, hover, loader, ...props }) => {
       {!loader ? <span className="btnContent">{children}</span> : <Loader />}
     </button>
   );
+};
+
+SuccessBtn.propTypes = {
+  /** Click handler. */
+  onClick: PropTypes.func,
+  /** Button fill. If set to `true`, the button's colors will "invert". */
+  fill: PropTypes.bool,
+  /** Hover effect. If set to `true`, the button will have a hover effect. */
+  hover: PropTypes.bool,
+  /** Loader effect. If set to `true`, will show a CSS loader instead of button text. */
+  loader: PropTypes.bool
+};
+
+SuccessBtn.defaultProps = {
+  onClick: null,
+  fill: false,
+  hover: false,
+  loader: false
 };
 
 export default SuccessBtn;
