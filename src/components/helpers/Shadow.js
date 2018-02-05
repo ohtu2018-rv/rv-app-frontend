63b66a9bed8e6920e2df32b432b83393ebf67dfc
alehuo <aleksi.huotala@helsinki.fi>
2018-02-05 22:58:31 +0200

    Added Shadow helper

---
+++ src/components/helpers/Shadow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles/Shadow.css';
+
+const Shadow = props => {
+    return <div className="shadow" />;
+};
+
+Shadow.propTypes = {};
+
+export default Shadow;
