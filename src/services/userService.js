921b509f0b55da4717aa5bbce028f58baaa4068e
laazz <lasse.vallius@helsinki.fi>
2018-02-09 22:23:30 +0200

    Add documentation for user service functions, remove unnecessary delay in login form

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,12 +2,21 @@
 
 const axios = require('axios');
 
+/**
+ * Fetches user information from backend.
+ * @param {string} token access token
+ */
 const getUser = async (token) => {
     return axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/account`, {
         headers: { 'Authorization': 'Bearer ' + token }
     }).then(res => res.data);
 }
 
+/**
+ * Reduces a user's account balance.
+ * @param {string} token access token
+ * @param {integer} amount amount to reduce
+ */
 const reduceBalance = async (token, amount) => {
     return axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/account/debit`, {
         amount: amount
@@ -18,6 +27,11 @@ const reduceBalance = async (token, amount) => {
     }).then(res => res.data.account_balance);
 }
 
+/**
+ * Increases a user's account balance.
+ * @param {string} token access token
+ * @param {integer} amount amount to increase
+ */
 const increaseBalance = async (token, amount) => {
     return axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/account/credit`, {
         amount: amount
