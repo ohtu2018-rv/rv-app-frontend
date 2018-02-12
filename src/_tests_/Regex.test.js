d4b88bcf350e0f61092e9cfc7a732ae8bd49b351
alehuo <aleksi.huotala@helsinki.fi>
2018-02-12 21:00:13 +0200

    Allow dots in deposits, e.g. d5.00

--- src/_tests_/Regex.test.js
+++ src/_tests_/Regex.test.js
@@ -1,29 +1,36 @@
 const regex =
-    '^d(255,00|0,(05|[1-9](0|5))|([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4]),[0-9](0|5))$';
+    '^d(255(.|,)00|0(.|,)(05|[1-9](0|5))|([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])(.|,)[0-9](0|5))$';
 
 describe('Money deposit regular expression', () => {
     it('Allows deposits from 0,05 to 255,00', () => {
         for (let i = 5; i <= 25500; i += 5) {
             const tmpPrice = parseFloat(i / 100)
                 .toFixed(2)
-                .toString()
-                .replace('.', ',');
+                .toString();
+            const tmpPrice2 = tmpPrice.replace(',', '.');
             const regexMatch = ('d' + tmpPrice).match(regex);
-            expect(regexMatch == null).toBe(false);
+            const regexMatch2 = ('d' + tmpPrice2).match(regex);
+            expect(regexMatch).not.toBe(null);
             expect(regexMatch[1]).toBe(tmpPrice);
+            expect(regexMatch2).not.toBe(null);
+            expect(regexMatch2[1]).toBe(tmpPrice2);
         }
     });
-    it('Doesn\'t allow a 0,00 deposit', () => {
+    it('Doesn\'t allow a 0,00 or 0.00 deposit', () => {
+        expect('d0,00'.match(regex)).toBe(null);
         expect('d0,00'.match(regex)).toBe(null);
     });
     it('Doesn\'t allow if d is missing', () => {
         expect('0,00'.match(regex)).toBe(null);
+        expect('0.00'.match(regex)).toBe(null);
     });
-    it('Doesn\'t allow if the amount is larger than 255,00', () => {
+    it('Doesn\'t allow if the amount is larger than 255,00 or 255.00', () => {
         expect('d255,05'.match(regex)).toBe(null);
         expect('d256,00'.match(regex)).toBe(null);
+        expect('d256.00'.match(regex)).toBe(null);
+        expect('d255.05'.match(regex)).toBe(null);
     });
-    it('Doesn\'t allow if no comma is entered', () => {
+    it('Doesn\'t allow if no comma or dot is entered', () => {
         expect('d255'.match(regex)).toBe(null);
         expect('d10'.match(regex)).toBe(null);
     });
